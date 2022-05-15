8ffab683e2d409f0ecfbf17e69f9eaa15cb992be
frenkyst <40049149+frenkyst@users.noreply.github.com>
2022-05-15 15:20:32 +0700

    Add files via upload

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		firstName string = "Menther"
+		lastName         = "Goreng"
+		value            = 999
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+}
