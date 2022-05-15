ebe3cd72a290d5788c9b54cfe0207a6f64415ac0
frenkyst <40049149+frenkyst@users.noreply.github.com>
2022-05-15 14:55:17 +0700

    Add files via upload

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Menther")
+	fmt.Println("Menther Susu")
+	fmt.Println("Menther SusuVerse")
+
+	fmt.Println(len("Menther"))
+	fmt.Println("Menther Susu"[0])
+	fmt.Println("Menther SusuVerse"[1])
+}
